1985baae78a9a2fffe99da9285e97d21a7a24895
So5ababy <oladka26@gmail.com>
2025-12-02 15:14:24 +0300

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("its work (^_^)");
+    }
+
+    public static int sum(int a, int b) {
+        return a + b;
+    }
+}
